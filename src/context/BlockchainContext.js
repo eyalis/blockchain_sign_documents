8d5ad9f38bab190e42ea05b2a18fb72ad87f6d42
Eyalis <eszewkis@yahoo.com>
2023-05-30 01:47:27 -0400

    fixed blockchain title issue that is configured in .env

--- src/context/BlockchainContext.js
+++ src/context/BlockchainContext.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-console.log("Blockchain:", process.env.REACT_APP_BLOCKCHAIN);
-console.log("Symbol:", process.env.REACT_APP_SYMBOL);
-
 // Create a context with default values
 const BlockchainContext = React.createContext({
     blockchain: process.env.REACT_APP_BLOCKCHAIN || 'Ethereum', // default blockchain
     symbol: process.env.REACT_APP_SYMBOL || 'ETH', // default symbol
 });
+
+console.log("Blockchain:", process.env.REACT_APP_BLOCKCHAIN);
+console.log("Symbol:", process.env.REACT_APP_SYMBOL);
 
 export default BlockchainContext;
